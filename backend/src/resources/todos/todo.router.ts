3e8e33d63140eb08f20ef87f9384bc20870568ec
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-15 16:56:46 +0500

    set post request route

--- backend/src/resources/todos/todo.router.ts
+++ backend/src/resources/todos/todo.router.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { Todo } from './todo.model';
 
+const todos: Todo[] = [];
 const todosRouter = Router();
 
 todosRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.json({
     message: 'Todo: Implement',
   });
+});
+
+todosRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const Itodo: Todo = {
+    id: Math.random().toString(36).substring(2, 7),
+    description: req.body.description,
+    isDone: false,
+  };
+  todos.push(Itodo);
+  res.json(Itodo);
 });
 
 export { todosRouter };
