bfc7668e5745d1f6cd41ad99f8d5cc7fd952f6d5
Ali Nauman <ali.nauman@aurorasolutions.io>
2022-09-15 11:48:53 +0500

    Create basic express app

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.info(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  res.json({
+    error: error.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.info(`Server running on port ${PORT}`);
+});
