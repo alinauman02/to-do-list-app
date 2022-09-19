30393a249c46b5c3147ca3ce4f636a1750736620
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-19 12:29:40 +0500

    change array to use state array

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
-import "./App.css";
 import Input from "./components/Input";
 import TodoList from "./components/TodoList";
 import { Todo } from "./models/todo.model";
-let todos: Todo[] = [];
-todos.push({ description: "Hello world", id: "244", isDone: true });
-todos.push({ description: "Hello Pakistan", id: "244", isDone: true });
-todos.push({ description: "Hello GB", id: "244", isDone: true });
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { description: "Hello world", id: "244454", isDone: true },
+    { description: "Hello Pakistan", id: "24454", isDone: true },
+    { description: "Hello GB", id: "24433", isDone: true },
+  ]);
+
   return (
     <div className="App">
-      <div className="card">
-        <Input /> <button id="bt">Post</button>
+      <div className=" card">
+        <div className="flex-container">
+          <Input /> <button className="inline-block">Post</button>
+        </div>
+
         <TodoList todos={todos} />
       </div>
     </div>
