f08b178344a49e63f17cd07f366fb77887abc6b9
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-19 15:33:55 +0500

    onchange and post task function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,11 +12,34 @@ function App() {
     { description: "Hello GB", id: "24433", isDone: true },
   ]);
 
+  let newtodos: Todo[];
+
+  let value: string = "";
+  const onChange = function (e: React.ChangeEvent<any>) {
+    newtodos = [
+      ...todos,
+      { description: e.target.value, id: "24445124", isDone: true },
+    ];
+  };
+
+  function postTask(): void {
+    setTodos(newtodos);
+  }
+
   return (
     <div className="App">
       <div className=" card">
         <div className="flex-container">
-          <Input /> <button className="inline-block">Post</button>
+          <Input
+            onChange={onChange}
+            value={value}
+            placeholder="ENTER TASK"
+            type="text"
+            name="todo"
+          />
+          <button className="inline-block" onClick={postTask}>
+            Post
+          </button>
         </div>
 
         <TodoList todos={todos} />
