55457503cca94183558caf0ccdd2de4d26fd03ab
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-19 16:00:44 +0500

    updated css

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
+import { useState } from "react";
+interface InputProps {
+  name: string;
+  value: string;
+  type: "number" | "text" | "email" | "password";
+  placeholder: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
 
-export default function Input(props: any) {
+export default function Input({
+  name,
+  value,
+  type,
+  placeholder,
+  onChange,
+}: InputProps) {
+  const [Value, setValue] = useState<string>(value);
   return (
     <input
-      height={props.accept}
-      width={props.width}
-      type={props.type}
-      placeholder={props.placeholder}
-      onChange={props.onChange}
+      value={Value}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      onChange={(e) => {
+        setValue(e.target.value);
+        onChange(e);
+      }}
     />
   );
 }
