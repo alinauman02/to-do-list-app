03a042dcb1d4be47f0ccb7aca0bba3afd38055db
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-20 14:33:47 +0500

    change any to todo

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { Todo } from "../models/todo.model";
 
 import TodoListItem from "./TodoListItem";
-
-export default function TodoList(props: any) {
+interface todoList {
+  todos: Todo[];
+}
+export default function TodoList(props: todoList) {
   return (
     <div>
       <ul className="ul-todos">
         {props.todos.map((todo: Todo) => (
-          <TodoListItem key={todo.id} todo={todo} />
+          <TodoListItem
+            key={todo.id}
+            description={todo.description}
+            id={todo.id}
+            isDone={todo.isDone}
+          />
         ))}
       </ul>
     </div>
--- frontend/src/components/TodoListItem.tsx
+++ frontend/src/components/TodoListItem.tsx
@@ -1,9 +1,10 @@
+import { Todo } from "../models/todo.model";
 import { IconDelete } from "./Icons/IconDelete";
-export default function TodoListItem(props: any) {
+export default function TodoListItem(props: Todo) {
   return (
     <li>
       <div className="list-item flex-container">
-        <p className="list-item-p">{props.todo.description}</p>
+        <p className="list-item-p">{props.description}</p>
         <button className="delete-button button">
           <IconDelete />
         </button>
