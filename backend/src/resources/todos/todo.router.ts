ad3159ec930ab5067fae5a7dcabb47a75d37ff86
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-21 17:16:21 +0500

    remove id and nextfunction

--- backend/src/resources/todos/todo.router.ts
+++ backend/src/resources/todos/todo.router.ts
@@ -1,10 +1,10 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import {  Request, Response, Router } from 'express';
 import { Todo } from './todo.model';
 
 let todos: Todo[] = [];
 const todosRouter = Router();
 
-todosRouter.get('/:id', (req: Request, res: Response) => {
+todosRouter.get('/', (req: Request, res: Response) => {
   res.json(todos);
 });
 
@@ -25,7 +25,7 @@ todosRouter.patch('/:id', (req: Request, res: Response) => {
   }
 });
 
-todosRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+todosRouter.post('/', (req: Request, res: Response) => {
   const todo: Todo = {
     id: Math.random().toString(36).substring(2, 7),
     description: req.body.description,
