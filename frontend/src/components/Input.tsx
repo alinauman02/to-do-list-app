2c339694ac29fa7cbc73c54e5b0b21863f0df708
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-21 11:21:58 +0500

    remove current value state

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,11 +1,11 @@
-import React, { useState, ChangeEvent } from "react";
+import { ChangeEvent } from "react";
 
 interface InputProps {
   name: string;
   value: string;
   type: "number" | "text" | "email" | "password";
   placeholder: string;
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (name: string, value: string) => void;
 }
 
 export default function Input({
@@ -15,17 +15,14 @@ export default function Input({
   placeholder,
   onChange,
 }: InputProps) {
-  const [currentValue, setCurrentValue] = useState<string>(value);
-
   const valueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setCurrentValue(event.target.value);
-    onChange(event);
+    onChange(event.target.name, event.target.value);
   };
 
   return (
     <input
       className="input-todo"
-      value={currentValue}
+      value={value}
       name={name}
       type={type}
       placeholder={placeholder}
