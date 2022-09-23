e28e812120ca4c5ac2e3924b8f9898eaa334c110
Ejaz hussain <ejazhussain1050@gmail.com>
2022-09-23 10:55:06 +0500

    fetch todos from endpoints

--- backend/src/resources/todos/todo.router.ts
+++ backend/src/resources/todos/todo.router.ts
@@ -1,13 +1,28 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { Todo } from './todo.model';
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 const todosRouter = Router();
 
 todosRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.json({
-    message: 'Todo: Implement',
-  });
+  res.json(todos);
+});
+
+todosRouter.delete('/del', (req: Request, res: Response, next: NextFunction) => {
+  if (todos.findIndex(todo => todo.id === req.query.id) !== -1) {
+    todos = todos.filter(todo => todo.id !== req.query.id);
+    res.json({ status: true });
+  } else {
+    res.json({ status: false });
+  }
+});
+
+todosRouter.put('/update', (req: Request, res: Response, next: NextFunction) => {
+  const index: number = todos.findIndex(todo => todo.id === req.query.id);
+  if (index !== -1) {
+    todos[index].isDone = !req.body.isDone;
+    res.json(todos[index]);
+  }
 });
 
 todosRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
